1a61b6c3cbccca3496c785d10747d68a17e8f555
tianjinran <43253477+tianjinran@users.noreply.github.com>
2020-10-28 22:25:04 -0700

    CSS assignment

--- src/ArticleList/ArticleList.jsx
+++ src/ArticleList/ArticleList.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 import ArticleListItem from "../ArticleListItem/ArticleListItem.jsx";
+import styles from "./ArticleList.module.css";
 
 const ArticleList = (props) => {
   let displayContent;
 
   if (props.articles.length) {
-    displayContent = (
-      <ul>
+      displayContent = (
+          <ul className={styles.container}>
         {props.articles.map((article) => (
           <ArticleListItem article={article} key={article.slug} />
         ))}
